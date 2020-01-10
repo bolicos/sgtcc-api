812ea9cda6fba3e33a3275300258ae4d69392abc
analuciabolico <analuciabolico.dev@gmail.com>
2020-01-10 18:10:26 -0300

    first commit

---
+++ src/test/java/com/analuciabolico/sgtccapi/SgtccApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analuciabolico.sgtccapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgtccApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
