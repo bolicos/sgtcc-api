55ad84579d1be366f948ee3bbd3ca08a54703589
analuciabolico <analuciabolico.dev@gmail.com>
2020-01-10 18:17:13 -0300

    refactor

--- src/main/java/com/analuciabolico/sgtccapi/Application.java
+++ src/main/java/com/analuciabolico/sgtccapi/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		Application.run(SgtccApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
