dc4fc2159e8cf34d9bf58c909901b8983c9e7eeb
analuciabolico <analuciabolico.dev@gmail.com>
2020-01-18 11:25:47 -0300

    add profiles application

--- src/main/java/com/analuciabolico/sgtccapi/v1/config/WebConfig.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/config/WebConfig.java
@@ -1,9 +1,24 @@
 package com.analuciabolico.sgtccapi.v1.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/h2/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/h2/**")
+                .allowCredentials(false);
+    }
 }
