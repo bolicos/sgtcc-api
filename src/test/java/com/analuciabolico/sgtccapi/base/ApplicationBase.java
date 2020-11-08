bede6964978dc2e537ee06178e8301fcf4a8799a
analuciabolico <analuciabolico.dev@gmail.com>
2020-11-07 23:50:33 -0300

    disabled test

--- src/test/java/com/analuciabolico/sgtccapi/base/ApplicationBase.java
+++ src/test/java/com/analuciabolico/sgtccapi/base/ApplicationBase.java
@@ -1,5 +1,6 @@
 package com.analuciabolico.sgtccapi.base;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApplicationBase {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 
 	}
