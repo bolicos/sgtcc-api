035f19401a35061fbac90c5aaa0f9ec63ba23ae8
analuciabolico <analuciabolico.dev@gmail.com>
2020-11-11 11:09:40 -0300

    add test and configuration classes

---
+++ src/test/java/com/analuciabolico/sgtccapi/v1/api/boards/BoardControllerIT.java
@@ -0,0 +1,10 @@
+package com.analuciabolico.sgtccapi.v1.api.boards;
+
+
+import com.analuciabolico.sgtccapi.v1.api.core.BaseControllerIT;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardControllerIT extends BaseControllerIT {
+
+}
---
+++ src/test/java/com/analuciabolico/sgtccapi/v1/api/core/BaseControllerIT.java
@@ -0,0 +1,23 @@
+package com.analuciabolico.sgtccapi.v1.api.core;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Tag("Slow")
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseControllerIT {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+
+}
