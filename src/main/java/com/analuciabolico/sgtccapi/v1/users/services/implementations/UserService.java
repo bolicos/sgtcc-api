5284bc2cefcf31a5be7d5f6b33b78fa06a44ec67
anakettl <anakettl@gmail.com>
2020-11-20 22:40:50 -0300

    feat(users): Remove crud and resources to user

--- src/main/java/com/analuciabolico/sgtccapi/v1/users/services/implementations/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.analuciabolico.sgtccapi.v1.users.services.implementations;
-
-import com.analuciabolico.sgtccapi.v1.users.services.interfaces.IUserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService implements IUserService {
-
-}
--- src/main/java/com/analuciabolico/sgtccapi/v1/users/services/interfaces/IUserService.java
+++
@@ -1,5 +0,0 @@
-package com.analuciabolico.sgtccapi.v1.users.services.interfaces;
-
-public interface IUserService {
-
-}
