eee124376efc02ae743cdb257b60861fc7a622a6
Ana Lucia Bolico <51336956+analuciabolico@users.noreply.github.com>
2020-11-22 12:53:18 -0300

    Update src/main/java/com/analuciabolico/sgtccapi/v1/interest_areas/dtos/Interest_AreaRequest.java

--- src/main/java/com/analuciabolico/sgtccapi/v1/interest_areas/dtos/Interest_AreaRequest.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/interest_areas/dtos/Interest_AreaRequest.java
@@ -9,7 +9,7 @@ public class InterestAreaRequest implements Serializable {
     private String name;
     private String description;
 
-    public Interest_Area convertToInterest_Area() {
+    public InterestArea convertToInterestArea() {
         return  Interest_Area.builder()
                 .id(null).description(this.description).name(this.name).createdAt(LocalDateTime.now())
                 .build();
