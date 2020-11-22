96e229f92bb2ce235952e3d3705a30370289a17e
anakettl <anakettl@gmail.com>
2020-11-22 15:07:35 -0300

    feat(plsql): add function return teacher title

--- src/main/java/com/analuciabolico/sgtccapi/v1/teachers/repository/TeacherRepository.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/teachers/repository/TeacherRepository.java
@@ -1,10 +1,17 @@
 package com.analuciabolico.sgtccapi.v1.teachers.repository;
 
 import com.analuciabolico.sgtccapi.v1.teachers.model.Teacher;
+import io.micrometer.core.lang.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "EXEC FUNCTION RETURN_TEACHER_TITLE(:#{Teacher.id})");
 }
