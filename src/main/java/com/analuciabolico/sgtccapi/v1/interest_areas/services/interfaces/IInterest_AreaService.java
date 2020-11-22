2df44a1ac0e489413ea530c89312abf552c8d3af
Ana Lucia Bolico <51336956+analuciabolico@users.noreply.github.com>
2020-11-22 12:50:24 -0300

    Update src/main/java/com/analuciabolico/sgtccapi/v1/interest_areas/services/interfaces/IInterest_AreaService.java

--- src/main/java/com/analuciabolico/sgtccapi/v1/interest_areas/services/interfaces/IInterest_AreaService.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/interest_areas/services/interfaces/IInterest_AreaService.java
@@ -1,7 +1,7 @@
 package com.analuciabolico.sgtccapi.v1.interest_areas.services.interfaces;
 
 import com.analuciabolico.sgtccapi.v1.core.models.ResourceCreated;
-import com.analuciabolico.sgtccapi.v1.interest_areas.dtos.Interest_AreaRequest;
+import com.analuciabolico.sgtccapi.v1.interest_areas.dtos.InterestAreaRequest;
 import com.analuciabolico.sgtccapi.v1.interest_areas.model.Interest_Area;
 import org.springframework.data.domain.Sort;
 
