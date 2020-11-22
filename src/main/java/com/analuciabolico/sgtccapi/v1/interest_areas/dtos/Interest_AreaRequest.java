d146316fdba20f618cf5b89aa11042bc9b766b05
anakettl <anakettl@gmail.com>
2020-11-22 12:20:39 -0300

    feat(interest-area): Add column definition to created at

--- src/main/java/com/analuciabolico/sgtccapi/v1/interest_areas/dtos/Interest_AreaRequest.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/interest_areas/dtos/Interest_AreaRequest.java
@@ -3,15 +3,15 @@ package com.analuciabolico.sgtccapi.v1.interest_areas.dtos;
 import com.analuciabolico.sgtccapi.v1.interest_areas.model.Interest_Area;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class Interest_AreaRequest implements Serializable {
     private String name;
-    private String createdAt;
     private String description;
 
     public Interest_Area convertToInterest_Area() {
         return  Interest_Area.builder()
-                .id(null).description(this.description).name(this.name).createdAt(this.createdAt)
+                .id(null).description(this.description).name(this.name).createdAt(LocalDateTime.now())
                 .build();
     }
 }
