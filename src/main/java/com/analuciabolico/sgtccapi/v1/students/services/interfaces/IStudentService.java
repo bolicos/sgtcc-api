a2afd739579ad902bc6b6be269fef7e867af0a92
anakettl <anakettl@gmail.com>
2020-11-23 16:04:11 -0300

    feat(plsql): Change return to StudentProposalTitleResponse

--- src/main/java/com/analuciabolico/sgtccapi/v1/students/services/interfaces/IStudentService.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/students/services/interfaces/IStudentService.java
@@ -1,6 +1,7 @@
 package com.analuciabolico.sgtccapi.v1.students.services.interfaces;
 
 import com.analuciabolico.sgtccapi.v1.core.models.ResourceCreated;
+import com.analuciabolico.sgtccapi.v1.students.dtos.StudentProposalTitleResponse;
 import com.analuciabolico.sgtccapi.v1.students.dtos.StudentRequest;
 import com.analuciabolico.sgtccapi.v1.students.model.Student;
 import org.springframework.data.domain.Sort;
@@ -14,5 +15,5 @@ public interface IStudentService {
 
     List<Student> findAll(Sort sort);
 
-    String findTitleProposalByStudent(Long id);
+    StudentProposalTitleResponse findTitleProposalByStudent(Long id);
 }
