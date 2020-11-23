debc026e8daa165d82a855f7094b71a05c3b46b5
Ana Lucia Bolico <51336956+analuciabolico@users.noreply.github.com>
2020-11-22 22:13:30 -0300

    add doc swagger-ui

--- src/main/java/com/analuciabolico/sgtccapi/v1/config/SwaggerConfig.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/config/SwaggerConfig.java
@@ -1,9 +1,31 @@
 package com.analuciabolico.sgtccapi.v1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SwaggerConfig implements WebMvcConfigurer {
+@EnableSwagger2
+public class SwaggerConfig extends WebConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.analuciabolico.sgtccapi.v1.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
