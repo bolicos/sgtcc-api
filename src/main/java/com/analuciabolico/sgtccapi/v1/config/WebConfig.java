61b2454f4a0c64b39fd627d76add8014708229f9
analuciabolico <analuciabolico.dev@gmail.com>
2021-08-16 22:07:36 -0300

    fix cors and add mapping to /api/v1 into registry.

--- src/main/java/com/analuciabolico/sgtccapi/v1/config/WebConfig.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.analuciabolico.sgtccapi.v1.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -18,10 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    @Profile("!prd")
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/h2/**")
-                .allowCredentials(false);
+        registry.addMapping("/h2/**").allowCredentials(false);
+        registry.addMapping("/api/v1/**").allowCredentials(false).allowedHeaders("*")
+                .allowedOrigins("https://sgtcc-front.herokuapp.com", "http://localhost:3000");
 
     }
+
 }
