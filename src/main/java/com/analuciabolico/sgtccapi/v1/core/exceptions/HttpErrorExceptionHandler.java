bf00fea1de3bfa87cf94c3189aacc4465f361c68
analuciabolico <analuciabolico.dev@gmail.com>
2021-08-18 19:56:46 -0300

    add banner and add message into exception of the error handler

--- src/main/java/com/analuciabolico/sgtccapi/v1/core/exceptions/HttpErrorExceptionHandler.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/core/exceptions/HttpErrorExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.analuciabolico.sgtccapi.v1.core.exceptions;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,22 +15,26 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class HttpErrorExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final Logger LOG = Logger.getLogger(HttpErrorExceptionHandler.class.getName());
 
     @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-        String body = "This should be application specific";
+        LOG.log(Level.WARNING, "Exception Conflict: {0}", ex.toString());
+        final String body = "This should be application specific" + ex.getMessage();
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
     @ExceptionHandler(value = { InternalServerError.class })
     protected ResponseEntity<Object> handleInternalServerError(RuntimeException ex, WebRequest request) {
-        String body = "Internal server Error";
+        LOG.log(Level.WARNING, "Exception Internal Server Error: {0}", ex.toString());
+        final String body = "Internal server Error" + ex.getMessage();
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
     @ExceptionHandler(value = { NotFoundException.class })
     protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request) {
-        String body = "Not Found Error";
+        LOG.log(Level.WARNING, "Exception Not Found Error: {0}", ex.toString());
+        final String body = "Not Found Error" + ex.getMessage();
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 }
