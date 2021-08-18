0de0743f5fe85443f1e6cbfc9a48243f90e01b18
analuciabolico <analuciabolico.dev@gmail.com>
2021-08-18 03:39:47 -0300

    add exceptions handlers

--- src/main/java/com/analuciabolico/sgtccapi/v1/core/exceptions/HttpErrorExceptionHandler.java
+++ src/main/java/com/analuciabolico/sgtccapi/v1/core/exceptions/HttpErrorExceptionHandler.java
@@ -1,5 +1,33 @@
 package com.analuciabolico.sgtccapi.v1.core.exceptions;
 
-public class HttpErrorExceptionHandler {
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpServerErrorException.InternalServerError;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
+public class HttpErrorExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler(value = { InternalServerError.class })
+    protected ResponseEntity<Object> handleInternalServerError(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Internal server Error";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler(value = { NotFoundException.class })
+    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Internal server Error";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
 }
